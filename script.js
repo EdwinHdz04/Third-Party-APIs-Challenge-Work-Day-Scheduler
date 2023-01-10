f8af1c0f3f0b5c6d40d696e07f928d8252e52f71
Edwin Hernandez <edwinhernandez80@icloud.com>
2023-01-09 18:43:03 -0800

    created and set time fucntion

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
+alert(dayjs().format('YYYY-MM-DD HH:mm:ss'))
+
+$(function() {
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -20,5 +23,13 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
+    function updateTime(){
+        var todaysDate = $("#currentDay");
+        todaysDate.text(dayjs().format('YYYY-MM-DD HH:mm:ss'))
+    
+    }
+    updateTime()
+    setInterval(updateTime,1000);
   });
+  
   
