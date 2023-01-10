d16a7066cffa8b24a5cf3d75e21600ae9784c1e9
Edwin Hernandez <edwinhernandez80@icloud.com>
2023-01-09 19:33:15 -0800

    save user input to local storage.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-alert(dayjs().format('YYYY-MM-DD HH:mm:ss'))
+
 
 $(function() {
     // TODO: Add a listener for click events on the save button. This code should
@@ -11,7 +11,16 @@ $(function() {
     // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
-    //
+    $('.saveBtn').on(" click", function(){
+        hour = $(this).parent().attr('id');
+        console.log(hour);
+        description = $(this).siblings(".description").val();
+        console.log(description);
+        localStorage.setItem(hour, description);
+
+    });
+    
+    
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
@@ -21,11 +30,14 @@ $(function() {
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
+
     //
     // TODO: Add code to display the current date in the header of the page.
     function updateTime(){
+        //var advancedFormat = require('dayjs/plugin/advancedFormat')
+        //dayjs.extend(advancedFormat)
         var todaysDate = $("#currentDay");
-        todaysDate.text(dayjs().format('YYYY-MM-DD HH:mm:ss'))
+        todaysDate.text(dayjs().format("dddd, MMMM DD YYYY, h:mm.ss"))
     
     }
     updateTime()
